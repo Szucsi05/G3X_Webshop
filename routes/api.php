cbc992bbed022cc91f3bece6f25f97c023246fcc
farkasbalazs25 <farkas.balazs@blathy.info>
2026-04-16 12:28:15 +0200

    api fixed

--- routes/api.php
+++ routes/api.php
@@ -9,55 +9,51 @@ use App\Http\Controllers\PlatformController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Basic API endpoints for managing products (games).
-// Protect these routes by sending a valid X-API-TOKEN header.
+
 
 Route::prefix('v1')->group(function () {
-    // Auth - login endpoint (no authentication required)
+
     Route::post('/login', [AuthController::class, 'apiLogin']);
     
-    // Products (token validated in controller)
+
     Route::get('/products', [ProductApiController::class, 'index']);
     Route::get('/products/{id}', [ProductApiController::class, 'show']);
     Route::post('/products', [ProductApiController::class, 'store']);
     Route::put('/products/{id}', [ProductApiController::class, 'update']);
     Route::delete('/products/{id}', [ProductApiController::class, 'destroy']);
 
-    // Categories
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-    // Platforms
     Route::get('/platforms', [PlatformController::class, 'index']);
     Route::get('/platforms/{id}', [PlatformController::class, 'show']);
     Route::post('/platforms', [PlatformController::class, 'store']);
     Route::put('/platforms/{id}', [PlatformController::class, 'update']);
     Route::delete('/platforms/{id}', [PlatformController::class, 'destroy']);
 
-    // Vendors
+
     Route::get('/vendors', [VendorController::class, 'index']);
     Route::get('/vendors/{id}', [VendorController::class, 'show']);
     Route::post('/vendors', [VendorController::class, 'store']);
     Route::put('/vendors/{id}', [VendorController::class, 'update']);
     Route::delete('/vendors/{id}', [VendorController::class, 'destroy']);
 
-    // Product Offers
+
     Route::get('/product-offers', [ProductOfferController::class, 'index']);
     Route::get('/product-offers/{id}', [ProductOfferController::class, 'show']);
     Route::post('/product-offers', [ProductOfferController::class, 'store']);
     Route::put('/product-offers/{id}', [ProductOfferController::class, 'update']);
     Route::delete('/product-offers/{id}', [ProductOfferController::class, 'destroy']);
 
-    // Product Offers by Product
     Route::get('/products/{productId}/offers', [ProductOfferController::class, 'byProduct']);
 
-    // Product Offers by Vendor
+ 
     Route::get('/vendors/{vendorId}/offers', [ProductOfferController::class, 'byVendor']);
 
-    // Orders (requires authentication)
+
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/orders', [OrderController::class, 'index']);
         Route::get('/orders/{id}', [OrderController::class, 'show']);
